0c8b74ca1ef1f84c1262962789fd3df5da15a2d6
sukanya2712 <sukanyanaik511@gmail.com>
2023-04-20 01:17:15 +0530

    added

--- src/main/java/addressbook.java
+++ src/main/java/addressbook.java
@@ -13,14 +13,9 @@ public class addressbook {
 
             Statement stmt = null;
                 stmt= conn.createStatement();
-            stmt.executeUpdate("CREATE TABLE contact_table(id int not null auto_increment primary key,first_name varchar(30) not null,\n" +
-                    "                       last_name varchar(30) not null,\n" +
-                    "                       address varchar(30),\n" +
-                    "                       city varchar(30),\n" +
-                    "                       state varchar(30),\n" +
-                    "                        zip int,\n" +
-                    "                        phone_number long,\n" +
-                    "                        email varchar(40))");
+            stmt.executeUpdate("insert into contact_table(first_name,last_name,address,city,state,zip,phone_number,email)values(\"sukanya\",\"naik\",\"srishti complex\",\"virar\",\"maharshtra\",401303,9822045799,\"sukanyanaik@gmail.com\"),(\"ganesh\",\"patil\",\"agarwal complex\",\"vasai\",\"delhi\",401313,9768145666,\"ganesh@gmail.com\"),(\"tanushree\",\"rai\",\"tanuja complex\",\"borivali\",\"hariyana\",401113,9822044449,\"rai@gmail.com\"),\n" +
+                    "                                      (\"mahesh\",\"chavan\",\"gokul complex\",\"andheri\",\"kanrataka\",401311,9822066669,\"mahesh@gmail.com\"),(\"priya\",\"raut\",\"bhatia complex\",\"dadar\",\"tamilnadu\",401444,9822088899,\"priya@gmail.com\")");
+
             System.out.println("Created table in given database...");
             boolean val = stmt.execute("Select * from contact_table");
             System.out.println(val);
@@ -30,13 +25,13 @@ public class addressbook {
 //                  retrieve by column name
                     System.out.println("ID: " + rs.getInt("id"));
                     System.out.println(", Name: " + rs.getString("first_name"));
-                    System.out.println(", Name: " + rs.getString("last_name"));
-                    System.out.println(", Name: " + rs.getString("address"));
-                    System.out.println(", Name: " + rs.getString("city"));
-                    System.out.println(", Name: " + rs.getString("state"));
-                    System.out.println(", Name: " + rs.getInt("zip"));
-                    System.out.println(", Name: " + rs.getLong("phone_number"));
-                    System.out.println(", Name: " + rs.getString("email"));
+                    System.out.println(", lastName: " + rs.getString("last_name"));
+                    System.out.println(", address: " + rs.getString("address"));
+                    System.out.println(", city: " + rs.getString("city"));
+                    System.out.println(", state: " + rs.getString("state"));
+                    System.out.println(", zip: " + rs.getInt("zip"));
+                    System.out.println(", phonenumber: " + rs.getLong("phone_number"));
+                    System.out.println(", email: " + rs.getString("email"));
                     System.out.println();
                 }
             }else {
